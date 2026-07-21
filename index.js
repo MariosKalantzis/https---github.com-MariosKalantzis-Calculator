d31a08cc2a8cf97a5b5bfa079023a4ac032623c9
Marios Kalantzis <marios.kalan@gmail.com>
2026-07-21 22:05:14 +0300

    Update button class names and fix display logic in calculator

--- index.js
+++ index.js
@@ -14,32 +14,51 @@ const operate = (operator, list) => {
     if (!operation) {
         throw new Error("Unsupported operator");
     }
-    return list.reduce(operation);
+    let result = list.reduce(operation);
+    DisplayOperator.textContent = '';
+    secondDisplayNumber.textContent = '';
+    firstDisplayNumber.textContent = result;
+    dispOperator = null;
+    num2 = "";
+    num1 = result.toString();
 }
 
 const container = document.querySelector('.container');
 const display = document.getElementById("display");
 const numbers = document.querySelectorAll('.number');
-const operator = document.querySelectorAll('.operator');
+const operators = document.querySelectorAll('.operators');
 const operateBtn = document.getElementById('operate');
 const clearButton = document.querySelector('clear');
 const firstDisplayNumber = document.getElementById('first-display-number');
 const DisplayOperator = document.getElementById('display-operator');
 const secondDisplayNumber = document.getElementById('second-display-number');
 
-
 numbers.forEach((button) => {
     button.addEventListener("click", (e) => {
-        num1 = num1.concat(e.target.dataset.value);
-        firstDisplayNumber.textContent = num1;
-        if (!dispOperator === null) {
+        if (dispOperator !== null) {
             num2 = num2.concat(e.target.dataset.value);
             secondDisplayNumber.textContent = num2;
+        } else {
+            num1 = num1.concat(e.target.dataset.value);
+            firstDisplayNumber.textContent = num1;
         }
-        console.log(e.target.dataset.value);
     })
 });
 
+operators.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (num1 !== '' && num2 !== '' && dispOperator !== null) {
+            operate(dispOperator, [Number(num1), Number(num2)])
+            return
+        }
+        DisplayOperator.textContent = e.target.dataset.value;
+        dispOperator = e.target.dataset.value;
+    })
+})
 
+operateBtn.addEventListener('click', (e) => {
+    if (num1 !== '' && num2 !== '' && dispOperator !== null) {
+        operate(dispOperator, [Number(num1), Number(num2)])
+    }
+})
 
-// console.log(operate("+", [1, 5]));
